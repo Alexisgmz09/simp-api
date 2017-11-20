154b2dcd3ebbdff78132e71826ed3bfde6a89fb0
Cesar Betancourt <cesarebetan@gmail.com>
2017-11-19 18:16:28 -0600

    UPDATE Now historial view uses models and services, new design of historial

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,4 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import { ActuatorAModel } from '../models/actuatorA-model';
+import { ActuatorAService } from '../services/actuatorA.service';
+import { ActuatorCModel } from '../models/actuatorC-model';
+import { ActuatorCService } from '../services/actuatorC.service';
+import { ActuatorFModel } from '../models/actuatorF-model';
+import { ActuatorFService } from '../services/actuatorF.service';
+
+import { SensorCModel } from '../models/sensorC-model';
+import { SensorCService } from '../services/sensorC.service';
+import { SensorMModel } from '../models/sensorM-model';
+import { SensorMService } from '../services/sensorM.service';
+import { SensorLModel } from '../models/sensorL-model';
+import { SensorLService } from '../services/sensorL.service';
+import { SensorTModel } from '../models/sensorT-model';
+import { SensorTService } from '../services/sensorT.service';
 
 @Component({
   selector: 'app-history',
@@ -6,10 +22,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history.component.css']
 })
 export class HistoryComponent implements OnInit {
+  actuatorsA: ActuatorAModel[] = [];
+  actuatorsC: ActuatorCModel[] = [];
+  actuatorsF: ActuatorFModel[] = [];
 
-  constructor() { }
+  sensorsC: SensorCModel[] = [];
+  sensorsM: SensorMModel[] = [];
+  sensorsL: SensorLModel[] = [];
+  sensorsT: SensorTModel[] = [];
+
+  constructor(private actuatorAService: ActuatorAService, private actuatorCService: ActuatorCService,
+              private actuatorFService: ActuatorFService, private sensorCService: SensorCService,
+              private sensorMService: SensorMService, private sensorLService: SensorLService,
+              private sensorTService: SensorTService) {
+  }
 
   ngOnInit() {
+    this.actuatorAService.getActuatorsA().then(actuatorsA => this.actuatorsA = this.actuatorsA);
+    this.actuatorCService.getActuatorsC().then(actuatorsC => this.actuatorsC = this.actuatorsC);
+    this.actuatorFService.getActuatorsF().then(actuatorsF => this.actuatorsF = this.actuatorsF);
+
+    this.sensorCService.getSensorsC().then(sensorsC => this.sensorsC = this.sensorsC);
+    this.sensorMService.getSensorsM().then(sensorsM => this.sensorsM = this.sensorsM);
+    this.sensorLService.getSensorsL().then(sensorsL => this.sensorsL = this.sensorsL);
+    this.sensorTService.getSensorsT().then(sensorsT => this.sensorsT = this.sensorsT);
   }
 
 }
